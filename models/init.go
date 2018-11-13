6c2b6239dcc2902a42d13c262c40a5cffdcd4907
hwangseonu <hwangseonu12@naver.com>
2018-11-13 13:39:25 +0900

    heroku mongodb

--- models/init.go
+++ models/init.go
@@ -3,12 +3,16 @@ package models
 import (
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
+	"os"
 )
 
 var DB *mgo.Database
 
 func init() {
-	if s, err := mgo.Dial("mongodb://localhost:27017/backend"); err != nil {
+	uri := os.Getenv("mongodb")
+	usernme := os.Getenv("mgo-username")
+	password := os.Getenv("mgo-password")
+	if s, err := mgo.Dial(uri+"/backend"); err != nil {
 		panic(err)
 	} else {
 		DB = s.DB("backend")
