62ae5dbe9d555e5a2afed4205e7344c8f79a838d
hwangseonu <hwangseonu12@naver.com>
2018-11-13 13:50:43 +0900

    use environment variable

--- models/init.go
+++ models/init.go
@@ -3,12 +3,13 @@ package models
 import (
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
+	"os"
 )
 
 var DB *mgo.Database
 
 func init() {
-	if s, err := mgo.Dial("mongodb://localhost:27017/backend"); err != nil {
+	if s, err := mgo.Dial(os.Getenv("MONGODB_URI")); err != nil {
 		panic(err)
 	} else {
 		DB = s.DB("backend")
